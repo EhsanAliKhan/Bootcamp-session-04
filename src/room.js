28aed51f720fca9526de889b5d64658f7ef69558
EhsanAliKhan <64222974+EhsanAliKhan@users.noreply.github.com>
2020-08-08 21:27:52 +0500

    new className added

--- src/room.js
+++ src/room.js
@@ -6,13 +6,15 @@ function Room() {
   let [count, setCount] = useState(1);
     
   return (
-    <div className={`room ${isLit? 'lit':'dark'}`}>
-      This Room is {isLit? "lit":"dark"}
-      <br/>
-      Age: {count}
-      <br/>
-      <button onClick={()=>setLit(!isLit)}>Update Time</button>
-      <button onClick={()=>setCount(++count)}>Update Age</button>
+    <div className ="complete">
+      <div className={`room ${isLit? 'lit':'dark'}`}>
+        This Room is {isLit? "lit":"dark"}
+        <br/>
+        Age: {count}
+        <br/>
+        <button onClick={()=>setLit(!isLit)}>Update Time</button>
+        <button onClick={()=>setCount(++count)}>Update Age</button>
+      </div>
     </div>
   );
 }
